c097551e034781c18d3e3c7473af2ee813a224ba
Dani Latorre <dani@danilat.com>
2020-11-26 20:40:43 +0100

    Fixing the assert_called for save method in the process video test

--- tests/unit/app/test_process_video.py
+++ tests/unit/app/test_process_video.py
@@ -12,7 +12,7 @@ def test_process_video_should_read_the_file(process_video, storage):
 def test_process_video_should_persist_the_video_as_received(process_video, video_repository):
     process_video(video_id = VIDEO_ID, storage_key = STORAGE_KEY)
 
-    video_repository.save.assert_called
+    video_repository.save.assert_called()
     persisted_video = video_repository.save.call_args.args[0]
     assert persisted_video.external_video_id == VIDEO_ID
     assert persisted_video.storage_key == STORAGE_KEY
